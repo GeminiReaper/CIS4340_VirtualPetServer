dcfe01b9a1dbfc0f5e04dcef0837b4f11292892a
Matthew Findlater <matthew.findlater@gmail.com>
2014-11-03 11:41:22 -0500

    initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = parse_ini_file('config.ini');
+
+$connectionString = $config['connectionString'];
+$username = $config['username'];
+$password = $config['password'];
+
+?>
---
+++ create_user.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('config.php');
+
+try {
+	$pdo = new PDO($connectionString, $username, $password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$stmt = $pdo->prepare('INSERT INTO User VALUES(:Username, :Email, :Password)');
+	$stmt->execute(array(
+    ':Username' => $_POST["username"],
+    ':Email' => $_POST["email"],
+    'Password' => password_hash($_POST["password"])
+    ));
+
+    $result = array(
+    	'result' => 'success'
+    );
+
+    echo json_encode($result);
+
+}
+catch(PDOException $e) {
+	echo 'Error' . $e->getMessage();
+}
+
+?>
